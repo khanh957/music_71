f40797ebcf54f742fc2ee4a79a1e694652f273a6
Khanh <baokhanh1008@gmail.com>
2019-09-16 15:49:47 +0700

    Initialize project structure

---
+++ app/src/main/java/com/sun_asterisk/soundcloud_71/MainActivity.java
@@ -0,0 +1,13 @@
+package com.sun_asterisk.soundcloud_71;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
